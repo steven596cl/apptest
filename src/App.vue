6a2900f772786bed4928b0901c1053ac5097d4c7
steven596cl <877568762@qq.com>
2021-07-06 21:43:33 +0800

    项目初始化-调整初始目录结构

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <h1>这是首页</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+</style>
